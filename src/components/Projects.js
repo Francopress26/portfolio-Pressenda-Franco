95f4a8a181b4c50e956d14fc576d6f9c0b343ecb
Franco Pressenda <francopress26@hotmail.com>
2022-09-17 00:34:25 -0300

    Portfolio finished

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -73,7 +73,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>These are the projects I have done, all were done in the Bootcamp Soy Henry. They made me gain a lot of experience in the technologies and also were useful to practice my soft skills with teamwork, doing them was a beautiful experience ❤</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
